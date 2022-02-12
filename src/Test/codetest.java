bd684cd96dab820a4540fb47cb1e2f0d3f424fde
Fa-NCR-22 <98920334+Fa-NCR-22@users.noreply.github.com>
2022-02-12 21:44:22 +0100

    2ème commit

--- src/Test/codetest.java
+++ src/Test/codetest.java
@@ -3,6 +3,6 @@ package Test;
 public class codetest {
        public static void main(String[] args) {
     	   System.out.print("hello fatima zahra");
-    	   System.out.print("how are you ");
+    	   System.out.print("how are you 12344");
        }
 }
